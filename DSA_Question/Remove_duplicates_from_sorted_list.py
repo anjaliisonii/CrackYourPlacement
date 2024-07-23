ce5e7b3c925893cf7c5f65b5f0746d2afe4659fe
anjaliisonii <anjalisoni102003@gmail.com>
2024-07-23 23:10:09 +0530

    Day7

---
+++ DSA_Question/Remove_duplicates_from_sorted_list.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head==None:
+            return head
+        node1=head
+        while node1.next:
+            if (node1.val==node1.next.val):
+                node1.next=node1.next.next
+            else:
+                node1=node1.next
+        return head
+        
